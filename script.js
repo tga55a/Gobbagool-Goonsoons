cafb297756eabc4a308e8a60e1f850536b4029af
Rahul Murgai <ramurgai@my.waketech.edu>
2025-05-05 11:35:39 -0400

    yippee

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 let bank = document.getElementById("bank")
 let gobbagool = document.getElementById("gobbagool")
 gobbagool.addEventListener("click", gobfunc)
+
+let Buttons = document.getElementById("Buttons")
 let gobs = 0
 let rate = 0
-
-//All the tralalero tralala stuff
-let tralcost = 5
-let tralowned = 0
-
-let tralalero = document.getElementById("tralalero")
-tralalero.addEventListener("click", tralalerofunc)
-tralalero.textContent = `Tralalero Tralala: Cost = ${tralcost}`
 
 
 function tralalerofunc(){
@@ -29,6 +23,73 @@ function tralalerofunc(){
     }
 }
 
+
+class Pet {
+    constructor(PetName, Cost, Rate, divisible) {
+
+        let Button = document.createElement("button")
+        Button.textContent = PetName
+
+
+        this.PetName = PetName
+        this.Cost = Cost
+        this.Owned = 0
+        this.rate = Rate
+        this.divisible = divisible
+        this.Button = Button
+
+        let update = this.update()
+
+        Button.addEventListener("click", update)
+
+        Buttons.appendChild(Button)
+    }
+
+    update() {
+        console.log("workinggg")
+        let Owned = this.Owned
+        let Cost = this.Cost
+        let Rate = this.rate
+        
+        return function() {
+            if (gobs >= Cost){
+                // Update
+                rate += Rate
+                gobs -= Cost
+                this.Owned += 1
+    
+                // Display
+                bank.textContent = gobs.toFixed(2)
+                console.log("tral owned " + this.Owned)
+            }
+            if (this.Owned % this.divisible === 0 && this.Owned !== 0){
+                this.Cost += 1
+                this.Button.textContent =  `${this.PetName}: Cost = ${this.Cost}`
+            }
+        }
+
+        
+    }
+}
+
+let Pets = {
+    "Tralalero Tralala": new Pet("Tralalero Tralala", 5, 0.1, 5),
+    "Bombardiro Crocodilo": new Pet("Bombardiro Crocodilo"),
+    "Brr Brr Patapim": new Pet("Brr Brr Patapim"),
+    "Boneca Ambalabu": new Pet("Boneca Ambalabu"),
+    "Bobrito Bandito": new Pet("Bobrito Bandito"),
+    "Lirili Larila": new Pet("Lirili Larila"),
+    "Ballerina Capuccina": new Pet("Ballerina Capuccina"),
+    "Cupuccino Assasino": new Pet("Cupuccino Assasino"),
+    "Tung Tung Tung Tung Tung Tung Tung Tung Tung Sahur": new Pet("Tung Tung Tung Tung Tung Tung Tung Tung Tung Sahur"),
+    "Chimpanzini Bananini": new Pet("Chimpanzini Bananini"),
+}
+
+for (Pet in Pets) {
+    console.log(Pet)
+}
+
+
 function gobfunc(){
     gobs += 1
     bank.textContent = gobs.toFixed(2)
@@ -41,8 +102,3 @@ function increase(){
 }
 
 increase()
-
-
-
-
-
